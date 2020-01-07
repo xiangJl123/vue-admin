040f1cc6b9c40f130f8c2ccaea3f338133eaef20
xiangjinlu <xiangjinlu@curefun.com>
2020-01-07 09:29:51 +0800

    新增全局样式

---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    redirect:"login"
+  },
+  // 重定向到login
+  {
+    path: "/login",
+    name: "Login",
+    component: () =>
+      import(/* webpackChunkName: "login" */ "../views/login/index")
+
+  },
+  {
+    path: "/about",
+    name: "about",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue")
+  }
+];
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;
---
+++ src/views/login/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        登录页面
+
+        <button @click="login">登录按钮</button>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            msg:"登录页面"
+        }
+    },
+    methods:{
+        login:function(){
+
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+    button{
+        margin-top: 300px;
+    }
+</style>
